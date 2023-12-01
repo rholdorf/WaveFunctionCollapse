cd688a5ecd33939479cbaa403047d5e10e3a6a64
Rui Holdorf <ruih@pbtech.net.br>
2023-12-01 18:29:30 -0300

    keep track of rectangles of original image

--- src/WaveFunctionCollapse/Tiles.cs
+++ src/WaveFunctionCollapse/Tiles.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -6,45 +7,95 @@ namespace WaveFunctionCollapse;
 
 public class Tiles
 {
-    private readonly Texture2D[] _entries;
-
+    private readonly Rectangle[] _rectangles;
+    private readonly int[] _indexes;
     private readonly int _max = 220;
+    private readonly Texture2D _texture;
     
-    public Tiles(Texture2D image, int tileWidth, int tileHeight, int tileOffsetVertical, int tileOffsetHorizontal)
+    public Tiles(Texture2D texture, int tileWidth, int tileHeight, int tileOffsetVertical, int tileOffsetHorizontal)
     {
-        var tw = (int)Math.Round((double)image.Width / (tileWidth + tileOffsetVertical));
-        var th = (int)Math.Round((double)image.Height / (tileHeight + tileOffsetHorizontal));
+        _texture = texture;
+        var tw = (int)Math.Round((double)texture.Width / (tileWidth + tileOffsetVertical));
+        var th = (int)Math.Round((double)texture.Height / (tileHeight + tileOffsetHorizontal));
         var tileCount = tw * th;
-        _entries = new Texture2D[tileCount];
+        _rectangles = new Rectangle[tileCount];
+        _indexes = new int[tileCount];
         var entryCount = 0;
 
-        for (var y = 0; y < image.Height; y += tileHeight + tileOffsetHorizontal)
+        for (var y = 0; y < texture.Height; y += tileHeight + tileOffsetHorizontal)
         {
-            for (var x = 0; x < image.Width; x += tileWidth + tileOffsetVertical)
+            for (var x = 0; x < texture.Width; x += tileWidth + tileOffsetVertical)
             {
-                var tileData = new Color[tileHeight * tileWidth];
-                image.GetData(0, new Rectangle(x, y, tileWidth, tileHeight), tileData, 0, tileData.Length);
-                _entries[entryCount] = new Texture2D(image.GraphicsDevice, tileWidth, tileHeight);
-                _entries[entryCount].SetData(tileData);
+                _rectangles[entryCount] = new Rectangle(x, y, tileWidth, tileHeight);
                 entryCount++;
             }
         }
+
+        FindUniqueTiles();
     }
 
-    public void Draw(SpriteBatch spriteBatch, Rectangle position)
+    public void Draw(SpriteBatch spriteBatch, Rectangle destinationRectangle)
     {
-        for (var i = 0; i < _entries.Length; i++)
+        for (var i = 0; i < _rectangles.Length; i++)
         {
-            var tile = _entries[i];
-            position.Height = tile.Height;
-            position.Width = tile.Width;
+            var tileIndex = _indexes[i];
+            //Console.WriteLine($"Tile index: {tileIndex}");
+            var sourceRectangle = _rectangles[i /*tileIndex*/];
+            destinationRectangle.Height = sourceRectangle.Height;
+            destinationRectangle.Width = sourceRectangle.Width;
 
-            spriteBatch.Draw(tile, position, Color.White);
-            position.X += tile.Width;
-            if (position.X <= _max/*GraphicsDevice.Viewport.Width*/) 
+            spriteBatch.Draw(_texture, destinationRectangle, sourceRectangle, Color.White);
+            
+            // move to next tile position
+            destinationRectangle.X += sourceRectangle.Width;
+            if (destinationRectangle.X <= _max) 
                 continue;
-            position.X = 0;
-            position.Y += tile.Height;
+            destinationRectangle.X = 0;
+            destinationRectangle.Y += sourceRectangle.Height;
         }        
+    }
+    
+    private void FindUniqueTiles()
+    {
+        var uniqueTiles = new List<Color[]>();
+        for (var index = 0; index < _rectangles.Length; index++)
+        {
+            var rectangle = _rectangles[index];
+            var tileData = new Color[rectangle.Width * rectangle.Height];
+            _texture.GetData(0, rectangle, tileData, 0, tileData.Length);
+            var exists = GetIndexFor(tileData, uniqueTiles);
+            if (exists == -1)
+            {
+                uniqueTiles.Add(tileData);
+                _indexes[index] = uniqueTiles.Count - 1;
+            }
+            else
+            {
+                _indexes[index] = exists;
+            }
+        }
+
+        Console.WriteLine($"{_texture.Name} unique tiles: {uniqueTiles.Count}");
+    }
+    
+    private static int GetIndexFor(IReadOnlyList<Color> tileData, IReadOnlyList<Color[]> uniqueTiles)
+    {
+        for (var index = 0; index < uniqueTiles.Count; index++)
+        {
+            var existingTile = uniqueTiles[index];
+            var isEqual = true;
+            for (var i = 0; i < tileData.Count; i++)
+            {
+                if (existingTile[i] == tileData[i]) 
+                    continue;
+                
+                isEqual = false;
+                break; // at least one pixel is different
+            }
+
+            if (isEqual) return index;
+        }
+
+        return -1;
     }
 }
